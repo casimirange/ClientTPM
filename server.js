be38a626c2666410bf959e15a64d311044efc2e8
casimirange <angecasimir41@gmail.com>
2020-10-21 06:50:24 +0100

    server.js

--- server.js
+++ server.js
@@ -8,3 +8,24 @@ const path = require("path");
 const request = require("request");
 
 app.use(express.static(__dirname + "/dist/ClientTPM"))
+
+app.listen(process.env.PORT || 4200)
+
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname + "/dist/ClientTPM/index.html"))
+})
+
+console.log("App is listening");
+// const wakeUpServerInterval = setInterval(wakeUpServer, 25 * 60 * 1000);
+//
+// function wakeUpServer() {
+//     const now = new Date();
+//     var endHour = new Date();
+//     endHour.setHours(25, 30, 0);
+//
+//     if(now < endHour){
+//         request(
+//
+//         )
+//     }
+// }
